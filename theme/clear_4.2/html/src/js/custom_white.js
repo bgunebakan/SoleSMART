cfb378ba81973999eac5b5631bdf8ba49f91bbd3
bgunebakan <bilaltonga@gmail.com>
2019-01-20 18:00:40 +0300

    last changes

--- theme/clear_4.2/html/src/js/custom_white.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-$(document).ready(function () {
-    $("body").addClass('light_layout');
-    if($("body").hasClass('light_layout')){
-        $(".navbar .logo img").attr('src','img/logo.png')
-    }else{
-        $(".navbar .logo img").attr('src','img/logo_white.png')
-    }
-});
